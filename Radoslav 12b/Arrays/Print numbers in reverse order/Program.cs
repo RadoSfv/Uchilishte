029cdd48eb801ef437b0a634ed82021405c66f51
RadoSfv <r.stefchev2017@pmg-pernik.com>
2021-10-07 12:22:48 +0300

    ff

---
+++ Radoslav 12b/Arrays/Print numbers in reverse order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Print_numbers_in_reverse_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Reverse(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ Radoslav 12b/Arrays/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                var arr = Console.ReadLine().Split(' ').ToArray();
+
+                Console.WriteLine(string.Join(" ", arr.Reverse()));
+            }
+        }
+    }
+}
